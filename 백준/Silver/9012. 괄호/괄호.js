f43de74f44d4ae8772e69ef58b95bfdc2afdb06f
Lee Ye Seo <rosefor2ne@gmail.com>
2025-05-29 09:30:29 +0900

    괄호 문제 풀이 방법 추가

--- 백준/Silver/9012. 괄호/괄호.js
+++ 백준/Silver/9012. 괄호/괄호.js
@@ -1,22 +1,47 @@
 const fs = require("fs");
 const [t, ...datas] = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
+// Stack
 for (let i = 0; i < Number(t); i++) {
-  const s = datas[i];
-  let balance = 0;
-  let valid = true;
+  const data = datas[i];
+  const stack = [];
 
-  for (const ch of s) {
-    if (ch === "(") {
-      balance++;
+  if (data[0] === ")" || data[data.length - 1] === "(") {
+    console.log("NO");
+    continue;
+  }
+
+  for (let j = 0; j < data.length; j++) {
+    const l = data[j];
+    if (l === "(") {
+      stack.push(l);
+    } else if (stack[stack.length - 1] === "(") {
+      stack.pop();
     } else {
-      balance--;
-    }
-    if (balance < 0) {
-      valid = false;
-      break;
+      stack.push(l);
     }
   }
 
-  console.log(valid && balance === 0 ? "YES" : "NO");
+  console.log(stack.length === 0 ? "YES" : "NO");
 }
+
+// Counting
+// for (let i = 0; i < Number(t); i++) {
+//   const s = datas[i];
+//   let balance = 0;
+//   let valid = true;
+
+//   for (const ch of s) {
+//     if (ch === "(") {
+//       balance++;
+//     } else {
+//       balance--;
+//     }
+//     if (balance < 0) {
+//       valid = false;
+//       break;
+//     }
+//   }
+
+//   console.log(valid && balance === 0 ? "YES" : "NO");
+// }
