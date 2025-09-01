af5b74bf704ac5d4a410693b9d2d7176e25a09dd
Lee Ye Seo <rosefor2ne@gmail.com>
2025-09-02 08:16:42 +0900

    [level 1] Title: 모의고사, Time: 21.21 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,21 +1,17 @@
 function solution(answers) {
-    const score = [0, 0, 0];
     const people = [
         [1, 2, 3, 4, 5],
         [2, 1, 2, 3, 2, 4, 2, 5],
         [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
     ];
     
-    answers.forEach((answer, i) => {
-        people.forEach((pattern, j) => {
-            if(answer === pattern[i % pattern.length]) {
-                score[j]++;
-            }
-        })
-    })
+	const scores = people.map(
+		(pattern) => answers.filter((answer, i) =>
+			answer === pattern[i % pattern.length]).length
+	);
     
-    const maxScore = Math.max(...score);
-    return score
+    const maxScore = Math.max(...scores);
+    return scores
         .map((score, idx) => (score === maxScore ? idx + 1 : null))
-        .filter((v) => v !== null);
+        .filter(Boolean);
 }
