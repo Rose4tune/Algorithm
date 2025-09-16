433dc646722bc2e113a01e5039ec3a4a0da4edce
Lee Ye Seo <rosefor2ne@gmail.com>
2025-09-17 07:46:56 +0900

    [level 2] Title: 짝지어 제거하기, Time: 46.12 ms, Memory: 43.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,13 @@
+function solution(s) {
+    if (!s) return 1;
+    
+    const stack = [];
+    
+    stack.push(s[0]);
+    for (let i = 1; i < s.length; i++) {
+        if (s[i] === stack[stack.length - 1]) stack.pop();
+        else stack.push(s[i]);
+    }
+    
+    return stack.length === 0 ? 1 : 0;
+}
