0ba8469f22d737193e428e601d70de3003a46325
Lee Ye Seo <rosefor2ne@gmail.com>
2026-01-15 23:29:20 +0900

    [level 0] Title: 문자열 출력하기, Time: 45.34 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.js
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.js
@@ -2,13 +2,5 @@ const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-});
+}).on('line', console.log);
 
-let input = [];
-
-rl.on('line', function (line) {
-    input = [line];
-}).on('close',function(){
-    str = input[0];
-    console.log(str)
-});
