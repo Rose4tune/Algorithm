2e2c43e405d9dd597baf9ec256a4720f25c4c6d5
Lee Ye Seo <rosefor2ne@gmail.com>
2026-01-16 15:32:12 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -0,0 +1,14 @@
+function solution(numer1, denom1, numer2, denom2) {
+    let numer = (numer1 * denom2) + (numer2 * denom1);
+    let denom = denom1 * denom2;
+    
+    const gcd = (a, b) => b === 0 ?  a : gcd(b, a % b);
+    const commonGcd = gcd(numer, denom);
+    
+    if(commonGcd > 1) {
+        numer = numer/commonGcd;
+        denom = denom/commonGcd;
+    }
+    
+    return [numer, denom]
+}
