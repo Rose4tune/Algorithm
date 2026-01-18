060ad1638a0feb71b7d0ead88bb7a4fe6fe40547
Lee Ye Seo <rosefor2ne@gmail.com>
2026-01-18 13:08:56 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,5 +1,3 @@
 function solution(n) {
-    return Array
-        .from({length: n + 1}, (_, i) => i)
-        .filter(n => n % 2 === 1)
+    return Array.from({length: ((n + 1) / 2)}, (_, i) => (i * 2) + 1)
 }
