dfa3112310b81383fedb9eeb55b5979c65c0b09e
Lee Ye Seo <rosefor2ne@gmail.com>
2026-03-27 07:14:16 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.06 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -1,14 +1,7 @@
 function solution(a, b, c) {
-    const square = (num, cnt) => {
-        for(let i = 1; i <= cnt; i++) {
-            if(i === cnt - 1) return num * num
-            return square(num, cnt - 1) * num
-        }
-    }
-    
     const normal = a + b + c;
-    const duble = square(a, 2) + square(b, 2) + square(c, 2);
-    const cube = square(a, 3) + square(b, 3) + square(c, 3);
+    const duble = Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2);
+    const cube = Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3);
     
     if(a === b && a === c && b === c) return normal * duble * cube;
     if(a != b && a != c && b != c) return normal;
